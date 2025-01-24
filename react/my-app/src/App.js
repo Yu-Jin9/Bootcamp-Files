069eb3d4c3ea80f995837d82f431c479d9dd725e
Yu-Jin9 <ujh50188@gmail.com>
2025-01-24 16:23:36 +0900

    0124 수업

---
+++ react/my-app/src/App.js
@@ -0,0 +1,6 @@
+function App() {
+  return <h1 className="greeting">Hello React</h1>;
+}
+
+// 기본은 App
+export default App;
---
+++ react/my-app/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client"; // client: FSR server:SSR(next.js)
+import App from "./App"; // 내가 render할 파일을 가져오기
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
